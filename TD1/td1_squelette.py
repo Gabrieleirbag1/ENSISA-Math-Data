058a3ff643238a364dbe30fb188d263c855f4fc9
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-12-08 15:37:48 +0100

    ex6

--- TD1/td1_squelette.py
+++ TD1/td1_squelette.py
@@ -11,25 +11,41 @@ from my_descent_td1 import GradientDescent
 
 
 ### EXERCICE 5
-def gradient_ex_5(x):
+print("Exercice 5")
+
+def f_ex5(x):
     return x**2 + 1
 
-ex5_a = GradientDescent(gradient_ex_5, 0.2, 10)
+def gradient_f_ex5(x):
+    return 2*x
+
+ex5_a = GradientDescent(gradient_f_ex5, 0.2, 10)
 image = ex5_a.descent(2)
-print(image)
+print("a) :", image)
 
-ex5_b = GradientDescent(gradient_ex_5, 0.01, 1000)
-ex5_b.descent(-1.5)
+ex5_b = GradientDescent(gradient_f_ex5, 0.01, 10)
+print("b) :", ex5_b.descent(-1.5))
 
-ex5_c1 = GradientDescent(gradient_ex_5, 0.9, 10)
-ex5_c1.descent(2)
-ex5_c2 = GradientDescent(gradient_ex_5, 1.1, 10)
-ex5_c2.descent(2)
-ex5_c3 = GradientDescent(gradient_ex_5, 0.05, 10)
-ex5_c3.descent(2)
+ex5_c1 = GradientDescent(gradient_f_ex5, 0.9, 10)
+print("c) 1 :", ex5_c1.descent(2))
+
+ex5_c2 = GradientDescent(gradient_f_ex5, 1.1, 10)
+print("c) 2 :", ex5_c2.descent(2))
+
+ex5_c3 = GradientDescent(gradient_f_ex5, 0.05, 10)
+print("c) 3 :", ex5_c3.descent(2))
 
 ### EXERCICE 6
+print("\nExercice 6")
 
+def f_ex6(x):
+    return x**4 - 2*x**3 + 4
+
+def gradient_f_ex6(x):
+    return 4*x**3 - 6*x**2
+
+ex6_a = GradientDescent(gradient_f_ex6, 0.05, 1000)
+print("a) :", ex6_a.descent(-1))
 
 ### EXERCICE 7 
 
